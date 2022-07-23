249c554f0a323d23275da501be4ea0c7c7a22b94
Guilherme R Galanti <galanti_imp@hotmail.com>
2022-07-23 17:10:56 -0300

    add viewbinding tecnich

--- app/src/main/java/com/guilhermegalanti/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/guilhermegalanti/a7minutesworkout/MainActivity.kt
@@ -4,17 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.guilhermegalanti.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener{
+        //val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener{
             Toast.makeText(this,
                 "Estou funcionando", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    // Resetar a variável binding
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null // Avoid memory leak
     }
 }
